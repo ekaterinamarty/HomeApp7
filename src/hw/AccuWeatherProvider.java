c0c7b9b345b8ebbd20c4a93b6c3781dd4b4167b2
Ekaterina <sonyamasonya@gmail.com>
2022-06-09 12:24:56 +0300

    Initial commit

---
+++ src/hw/AccuWeatherProvider.java
@@ -0,0 +1,90 @@
+package hw;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import hw.enums.Periods;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class AccuWeatherProvider implements WeatherProvider {
+    private static final String BASE_HOST = "dataservice.accuweather.com";
+    private static final String FORECAST_ENDPOINT = "forecasts";
+    private static final String CURRENT_CONDITIONS_ENDPOINT = "currentconditions";
+    private static final String API_VERSION = "v1";
+
+    private final OkHttpClient client = new OkHttpClient();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public AccuWeatherProvider() {
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    @Override
+    public void getWeather(Periods periods) throws IOException {
+        String cityKey = detectCityKey();
+        if (periods.equals(Periods.NOW)) {
+            HttpUrl url = new HttpUrl.Builder()
+                    .scheme("http")
+                    .host(BASE_HOST)
+                    .addPathSegment(CURRENT_CONDITIONS_ENDPOINT)
+                    .addPathSegment(API_VERSION)
+                    .addPathSegment(cityKey)
+                    .addQueryParameter("apikey", ApplicationGlobalState.getInstance().getApiKey())
+                    .build();
+
+            Request request = new Request.Builder()
+                    .addHeader("accept", "application/json")
+                    .url(url)
+                    .build();
+
+            Response response = client.newCall(request).execute();
+            System.out.println(response.body().string());
+            // TODO: Сделать в рамках д/з вывод более приятным для пользователя.
+            //  Создать класс WeatherResponse, десериализовать ответ сервера в экземпляр класса
+            //  Вывести пользователю только текущую температуру в C и сообщение (weather text)
+        }
+    }
+
+    public String detectCityKey() throws IOException {
+        String selectedCity = ApplicationGlobalState.getInstance().getSelectedCity();
+
+        HttpUrl detectLocationURL = new HttpUrl.Builder()
+                .scheme("http")
+                .host(BASE_HOST)
+                .addPathSegment("locations")
+                .addPathSegment(API_VERSION)
+                .addPathSegment("cities")
+                .addPathSegment("autocomplete")
+                .addQueryParameter("apikey", ApplicationGlobalState.getInstance().getApiKey())
+                .addQueryParameter("q", selectedCity)
+                .build();
+
+        Request request = new Request.Builder()
+                .addHeader("accept", "application/json")
+                .url(detectLocationURL)
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        if (!response.isSuccessful()) {
+            throw new IOException("Невозможно прочесть информацию о городе. " +
+                    "Код ответа сервера = " + response.code() + " тело ответа = " + response.body().string());
+        }
+        String jsonResponse = response.body().string();
+        System.out.println("Произвожу поиск города " + selectedCity);
+
+        if (objectMapper.readTree(jsonResponse).size() > 0) {
+            String cityName = objectMapper.readTree(jsonResponse).get(0).at("/LocalizedName").asText();
+            String countryName = objectMapper.readTree(jsonResponse).get(0).at("/Country/LocalizedName").asText();
+            System.out.println("Найден город " + cityName + " в стране " + countryName);
+        } else throw new IOException("Server returns 0 cities");
+
+        return objectMapper.readTree(jsonResponse).get(0).at("/Key").asText();
+    }
+}
